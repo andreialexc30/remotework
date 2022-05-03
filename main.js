50d877d7bb8062714e88bbd82488d2e0acbff74b
andreialexc30 <andreialexx30@gmail.com>
2022-05-03 13:12:10 +0300

    Init commit

---
+++ main.js
@@ -0,0 +1,35 @@
+// Select HTML elements
+const menuBtn = document.getElementById('mobile-menu_trigger');
+const closeMenuBtn = document.getElementById('mobile-menu_close');
+const menuList = document.querySelector('.mobile-nav-list');
+const dropdowns = document.querySelectorAll('.dropdown-trigger');
+const dropdownsList = document.querySelectorAll('.dropdown_list');
+
+// JS Events for DOM manipulation
+menuBtn.addEventListener('click', () => {
+    expandMenu('active')
+    menuBtn.style.display = 'none';
+    closeMenuBtn.style.display = 'initial'
+})
+
+closeMenuBtn.addEventListener('click', () => {
+    expandMenu('active')
+    menuBtn.style.display = 'initial';
+    closeMenuBtn.style.display = 'none';
+})
+
+dropdowns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        console.log(e.target)
+        let target = e.target.nextElementSibling;
+        target.classList.toggle('active')
+    })
+})
+
+function expandMenu(status) {
+    if(menuList.classList.contains(status)) {
+        menuList.classList.remove(status)
+    } else {
+        menuList.classList.add(status)
+    }
+}
